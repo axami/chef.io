4ec8065a3ea99bb7f14b1b461a238c07ef9f8145
Thomas Powell <powell@progress.com>
2022-11-01 11:31:58 -0400

    try testing turning on fips_mode
    Signed-off-by: Thomas Powell <powell@progress.com>

---
+++ spec/integration/client/fips_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+describe "chef-client fips", :windows_only do
+  PATH="HKLM:\\System\\CurrentControlSet\\Control\\Lsa\\FipsAlgorithmPolicy"
+  before do
+    @registry = Chef::Win32::Registry.new
+    @old_value = @registry.get_values("PATH")[0][:data]
+    @registry.set_value(PATH, @old_value.merge({data: 1})
+  end
+  after do
+    @registry.set_value(PATH, @old_value)
+  end
+
+  it "Should not error on enabling fips_mode" do
+    expect { OpenSSL.fips_mode = true }.not_to raise_error
+  end
+end
