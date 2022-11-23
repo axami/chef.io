ef289b69d466323feab5706b6bd8aab660caf6ea
Thomas Powell <powell@progress.com>
2022-11-23 10:44:12 -0500

    Break fips?
    Signed-off-by: Thomas Powell <powell@progress.com>

---
+++ spec/integration/client/fips_spec.rb
@@ -0,0 +1,11 @@
+describe "chef-client fips" do
+  def enable_fips_if_supported
+    if ENV["OMNIBUS_FIPS_MODE"].to_s.downcase == "true"
+      OpenSSL.fips_mode = true
+      OpenSSL.fips_mode = false
+    end
+  end
+  it "Should not error on enabling fips_mode" do
+    expect { enable_fips_if_supported }.not_to raise_error
+  end
+end
